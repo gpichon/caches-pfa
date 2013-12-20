f36964df1c719c974b3a3d283d30c1490871584c
Nicoeclair <nicoeclair@hotmail.fr>
2013-12-20 16:58:27 +0100

    ajout de la lecture d'une trace dans le main

--- idea_simulator/src/main.c
+++ idea_simulator/src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "add_line_hierarchy.h"
 #include "architecture.h"
+#include "trace.h"
 
 #define FILENAME "data/architest.xml"
 #define TEST "../gentraces/file.txt"
@@ -14,44 +15,42 @@ int main(int argc, char *argv[]) {
     strcpy(filename, argv[1]);
   }
   else {
-    strcpy(filename, FILENAME);
+    strcpy(filename, FILENAME); /* Trouver un nom plus générique */
   }
 
   struct architecture A;
   parse_archi_file(filename, &A);
   struct architecture *archi = &A;
-
-  int i = 0;
+  
+  struct thread *t;
+  struct instruction i = {.type = INSTRUCTION_INVALID};
+  
+  /* Open trace file */
+  char trace_file[] = "data/traces/s1113_trace";
+  t = create_thread(trace_file);
+  
+  if (!t) {
+    fprintf(stderr, "Cannot open trace file %s\n", trace_file);
+    return EXIT_SUCCESS;
+  }
+  
+  /* Read trace */
   int count = 0;
-
-  int j, k;
-  int taille = 100;
-  for (j=0; j<taille; j++){
-    for (k=0; k<2048; k++){
-      i++;
-      i = i%4;
-      store_line_hierarchy(archi, archi->threads[i], 6365296+1024*j+4*k);
+  while (i.type != INSTRUCTION_END_OF_THREAD) {
+    i = next_instruction(t);
+    if (i.type == INSTRUCTION_LOAD) {
+      load_line_hierarchy(archi, archi->threads[0], i.addr);
+      count++;
+    }
+    else if (i.type == INSTRUCTION_STORE) {
+      store_line_hierarchy(archi, archi->threads[0], i.addr);
       count++;
     }
   }
-  for (j=0; j<taille; j++){
-    for (k=0; k<2048; k++){
-      i++;
-      i = i%4;
-      load_line_hierarchy(archi, archi->threads[i], 636856+1024*j+4*k);
-      count++;
-    }
-  }
-  for (j=0; j<taille; j++){
-    for (k=0; k<2048; k++){
-      i++;
-      i = i%4;
-      store_line_hierarchy(archi, archi->threads[i], 6296+1024*j+4*k);
-      count++;
-    }
-  }
-
+  
+  free_thread(t);
   printf("Total values: %d\n", count);
+  /* End of reading */
 
   /* Informations about caches */
   print_caches(archi);
