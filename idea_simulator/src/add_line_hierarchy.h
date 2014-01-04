63a71e3408475bf6e4a609de6bd2b01b365d6abb
ahonorat <alexandre.honorat@yahoo.fr>
2014-01-04 13:55:14 +0100

    Seuls les getters/setters de line.h ne sont pas commentés, car suffisamment explicites

--- idea_simulator/src/add_line_hierarchy.h
+++ idea_simulator/src/add_line_hierarchy.h
@@ -21,7 +21,7 @@
 
 /**
  * \brief Used for a miss when load or a hit when store.    
- * \param \a cache is the cache which launched a share level procedure on parameter level.
+ * \param cache Is the cache which launched a share level procedure on parameter level.
  * \return Return 1 if there is entry is somewhere else in level.  
  */
 int share_level(struct list *level, struct cache *cache, int entry, void (*action)(struct line *));
