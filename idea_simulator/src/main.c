41deed6334a4eec4df7df2df9d5484277ec80fd4
Grégoire <gregoire@gregoire-Latitude-E5530-non-vPro.(none)>
2014-01-08 14:38:07 +0100

    Lecture de 4 traces dans le main, 10 intructions par trace. Idée pour la gestion future en Lua

--- idea_simulator/src/main.c
+++ idea_simulator/src/main.c
@@ -68,33 +68,64 @@ int main(int argc, char *argv[]) {
   parse_archi_file(filename, &A);
   struct architecture *archi = &A;
   
-  struct thread *t;
+  struct thread **t;
+  t = malloc(4*sizeof(struct thread *));
   struct instruction i = {.type = INSTRUCTION_INVALID};
   
   /* Open trace file */
-  char trace_file[] = "data/traces/s1113_trace";
-  t = create_thread(trace_file);
+  char trace_file1[] = "data/traces/trace1";
+  char trace_file2[] = "data/traces/trace2";
+  char trace_file3[] = "data/traces/trace3";
+  char trace_file4[] = "data/traces/trace4";
+
+
+  t[0] = create_thread(trace_file1);
+  t[1] = create_thread(trace_file2);
+  t[2] = create_thread(trace_file3);
+  t[3] = create_thread(trace_file4);
   
-  if (!t) {
-    fprintf(stderr, "Cannot open trace file %s\n", trace_file);
+  if (!t[0] || !t[1] || !t[2] || !t[3]) {
+    fprintf(stderr, "Cannot open trace files\n");
     return EXIT_SUCCESS;
   }
   
   /* Read trace */
   int count = 0;
-  while (i.type != INSTRUCTION_END_OF_THREAD) {
-    i = next_instruction(t);
-    if (i.type == INSTRUCTION_LOAD) {
-      load_line_hierarchy(archi, archi->threads[0], i.addr);
-      count++;
-    }
-    else if (i.type == INSTRUCTION_STORE) {
-      store_line_hierarchy(archi, archi->threads[0], i.addr);
-      count++;
+  int j;
+  int current = 3;
+  int end[4] = {0};
+
+  while (!end[0] || !end[1] || !end[2] || !end[3]){
+    current = (current+1)%4; 	/* Next thread */
+    for (j=0; j<10; j++){ 	/* 10 instructions per thread */
+      
+      if (i.type != INSTRUCTION_END_OF_THREAD) {
+	i = next_instruction(t[current]);
+	if (i.type == INSTRUCTION_LOAD) {
+	  load_line_hierarchy(archi, archi->threads[current], i.addr);
+	  count++;
+	}
+	else if (i.type == INSTRUCTION_STORE) {
+	  store_line_hierarchy(archi, archi->threads[current], i.addr);
+	  count++;
+	}
+      }
+      else{
+	end[current] = 1;
+	i.type = INSTRUCTION_INVALID;
+      }
     }
   }
+
   
-  free_thread(t);
+  free_thread(t[current]);
+  for (j=0; j<4; j++){
+    if (j!=current){
+      free(t[j]);
+    }
+  }
+  free(t);
+
   printf("Total values: %d\n", count);
   /* End of reading */
 
