51e1b3f6f3d3d532cd591f0bbc46a4d5299f5b30
gpichon <gpichon@localhost.localdomain>
2014-02-07 12:17:25 +0100

    Ajout du rapport de la reunion

---
+++ docs/reunions/5-respo.cr
@@ -0,0 +1,14 @@
+Réunion 7 Février 2014
+
+Problème pour tester:
+- compteurs hardware: compliqué
+
+Automates:
+- donner plus de confiance dans le code
+- dessiner les automates pour prendre le bon outil
+- jflap, smp
+- soit l'automate appelle le simulateur qui lui renvoit un machin ou inversement
+
+Enlever les #:
+- [^#]*.c
+- ^#*.c
