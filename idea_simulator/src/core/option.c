0f596a0c749d9289a6ad4c764aef165935ee6155
nicoeclair <nicoeclair@hotmail.fr>
2014-03-09 23:37:43 +0100

    commit de maj

--- idea_simulator/src/core/option.c
+++ idea_simulator/src/core/option.c
@@ -1,22 +1,48 @@
+/**
+ * \file option.c
+ * \brief define options
+ * \author ~ndubois
+ * \version 1.0
+ * \date 5th march 2014
+ * 
+ * 
+ *
+ */
 #include "option.h"
 
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+int tracking_type = TRACKING_DISABLED;
+int tracking_lower_bound;
+int tracking_upper_bound;
 int help = 0;
 char *trace_file = NULL;
 
 void get_options(int argc, char *argv[]) {
 	char c;
+	char *s;
 	
-	/* To add more options, add the letter and then ":" if the option is a string or nothing for a true/false statement */
-	while ((c = getopt(argc, argv, "f:h")) != -1)
+	while ((c = getopt(argc, argv, "f:hb:")) != -1)
 	{
 		switch (c)
 		{
-			case 'f':
+			case 'f':	/* -f architecture_file */
 				trace_file = optarg;
 				break;
-			case 'h':
+			case 'b':	/* -b 0x5555:0x6666 */
+				s = optarg;
+				if ((s = strchr(s, ':')) == NULL) {
+					fprintf(stderr, "Bad parameter form");
+					break;
+				}
+				tracking_lower_bound = strtol(optarg, &s, 16);
+				tracking_upper_bound = strtol(s+1, NULL, 16);
+				tracking_type = TRACKING_LIGHT;
+				break;
+			case 'h':	/* -h */
 				help = 1;
 				break;
 		}
--- idea_simulator/src/includes/option.h
+++ idea_simulator/src/includes/option.h
@@ -1,6 +1,53 @@
-#pragma once
+/**
+ * \file option.h
+ * \brief define options
+ * \author ~ndubois
+ * \version 1.0
+ * \date 5th march 2014
+ * 
+ * 
+ *
+ */
 
-extern int help;
-extern char *trace_file;
+#ifndef OPTION_H
+#define OPTION_H
 
+enum TrackingType { TRACKING_DISABLED, TRACKING_LIGHT, TRACKING_FULL };
+
+/**
+ * \def UP_TRACKING_HITS()
+ * \brief Count one hit in tracking stats.
+ */
+#define UP_TRACKING_HITS() tracking_hits++
+/**
+ * \def UP_TRACKING_MISSES()
+ * \brief Count one miss in tracking stats.
+ */
+#define UP_TRACKING_MISSES() tracking_misses++
+/**
+ * \def UP_TRACKING_WRITE_BACKS()
+ * \brief Count one write back in tracking stats.
+ */
+#define UP_TRACKING_WRITE_BACKS() tracking_write_backs++
+/**
+ * \def UP_TRACKING_BROADCASTS()
+ * \brief Count one broadcast in tracking stats.
+ */
+#define UP_TRACKING_BROADCASTS() tracking_broadcasts++
+
+extern int tracking_misses;      	/**< Count of misses for tracking. */
+extern int tracking_hits;        	/**< Count of hits for tracking. */
+extern int tracking_write_backs; 	/**< Count of write backs for tracking. */
+extern int tracking_broadcasts;  	/**< Count of broadcasts for tracking. */
+extern int tracking_type;	/**< Tracking type used during the simulation. */
+extern int tracking_lower_bound;	/**< Lower bound of address tracking. */
+extern int tracking_upper_bound;	/**< Upper bound of address tracking. */
+extern int help;					/**< help=1 when -h flag is set */
+extern char *trace_file;			/**< The artecture configuration file to use. If not set, a default file is used. */
+
+/**
+ * \brief Fill options variables with program's arguments
+ */
 void get_options(int argc, char *argv[]);
+
+#endif /* OPTION_H */
