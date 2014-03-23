c433bfcb3ee88c5e5c5df304b1e4ffdf3e3a3f04
ahonorat <alexandre.honorat@yahoo.fr>
2014-03-23 14:26:37 +0100

    vérification du .sm, le cmake échoue maintenant avec le message d'erreur adéquat quand le .sm est mal écrit

--- cassis/tests/non_regression_tests.sh
+++ cassis/tests/non_regression_tests.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-cmake ../../ -DTEST=ON
+cmake ../../ -DTEST=ON #-DLUA_LIBRARIES=-llua5.2 -DLUA_INCLUDE_DIR=/usr/include/lua5.2
 make
 
 cd ../non_regression
@@ -11,7 +11,7 @@ echo "Running some benchs"
 ./bench.sh > out 2>&1
 echo "Diff for non regression"
 diff out non_regression.txt > res_nr.txt
-echo "Diff in res.txt should print nothing"
+echo "Diff in non_regression/res_nr.txt should print nothing"
 rm out
 cd ..
 rm -rf build
