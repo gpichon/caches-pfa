6e4ebc790bff9d54a00f6d76ed77c6d16dae287e
ahonorat <alexandre.honorat@yahoo.fr>
2014-03-31 14:31:32 +0200

    Renommage, correction et reformatage en utf-8-unix

--- cassis/src/includes/add_line_hierarchy.h
+++ cassis/src/includes/add_line_hierarchy.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 Nicolas Dubois, Pierre Goudet, Nicolas Heng,
- * Alexandre Honorat, Gilles Marait, Grégoire Pichon.
+ * Alexandre Honorat, Gilles Marait, GrÃ©goire Pichon.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- cassis/src/includes/statemap.h
+++ cassis/src/includes/statemap.h
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2014 Nicolas Dubois, Pierre Goudet, Nicolas Heng,
- * Alexandre Honorat, Gilles Marait, Grégoire Pichon.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 #ifndef _H_STATEMAP
 #define _H_STATEMAP
 
